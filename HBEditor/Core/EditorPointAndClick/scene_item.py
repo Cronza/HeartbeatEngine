1a9d85a834e2c1c077a40ef561275c887cd5daea
Garrett Fredley <garrett.t.fredley@hotmail.com>
2021-12-03 22:52:36 -0800

    -Changing create_sprite default pos to 0.5, 0.5 -[Early, non-functional code for scene_item flipping

--- HBEditor/Core/EditorPointAndClick/scene_item.py
+++ HBEditor/Core/EditorPointAndClick/scene_item.py
@@ -18,12 +18,17 @@ class SceneItem(QtWidgets.QGraphicsPixmapItem):
         self.setFlag(self.ItemSendsGeometryChanges)
         self.setAcceptDrops(True)
 
+        # States
+        #self.is_centered = False
+        #self.is_flipped = False
+
         # Cache the index where the important action data elements are, so we don't iterate through each time
         # (This is on the assumption and knowledge that the order will never change)
         self.position_index = None
         self.sprite_index = None
         self.z_order_index = None
         self.center_align_index = None
+        self.flip_index = None
         for index, req in enumerate(self.action_data["requirements"]):
             if req["name"] == "position":
                 self.position_index = index
@@ -33,13 +38,22 @@ class SceneItem(QtWidgets.QGraphicsPixmapItem):
                 self.z_order_index = index
             elif req["name"] == "center_align":
                 self.center_align_index = index
+            elif req["name"] == "flip":
+                self.flip_index = index
 
     def Refresh(self):
         """
         Refresh is the common function used by elements that need refreshing when an important U.I change is made
         """
         #@TODO: Investigate making this more efficient
-        # *** Update the position ***
+
+        # Start with a fresh state, so any transforms apply properly
+        self.resetTransform()
+        new_transform = QtGui.QTransform()
+        hori_translation = new_transform.m31()
+        vert_translation = new_transform.m32()
+
+        # Update the position
         new_pos = self.action_data["requirements"][self.position_index]["value"]
 
         # Because the position is stored normalized between 0-1, inverse normalize the position value
@@ -47,7 +61,7 @@ class SceneItem(QtWidgets.QGraphicsPixmapItem):
             float(new_pos[0]) * self.scene().width(),
             float(new_pos[1]) * self.scene().height()
         )
-        # *** Update the sprite ***
+        # Update the sprite
         sprite_path = Settings.getInstance().user_project_dir + "/" + \
                       self.action_data["requirements"][self.sprite_index]["value"]
 
@@ -58,20 +72,40 @@ class SceneItem(QtWidgets.QGraphicsPixmapItem):
             else:
                 Logger.getInstance().Log(f"File does not Exist: '{sprite_path}'", 3)
 
-        # *** Update the z_order ***
+        # Update the z_order
         z_order = self.action_data["requirements"][self.z_order_index]["value"]
         self.setZValue(float(z_order))
 
-        # *** (OPTIONAL) Update center align ***
+        # (OPTIONAL) Update center align
         if self.center_align_index is not None:
             center_align = self.action_data["requirements"][self.center_align_index]["value"]
             if center_align:
-                self.setTransform(
-                    self.transform().translate(-self.boundingRect().width() / 2, -self.boundingRect().height() / 2))
-            else:
-                self.resetTransform()
+                print("Centering")
+                new_transform.translate(-self.boundingRect().width() / 2, -self.boundingRect().height() / 2)
+                hori_translation = new_transform.m31()
+                vert_translation = new_transform.m32()
+                print(hori_translation)
+                print(vert_translation)
 
+        # (OPTIONAL) Update flip
+        if self.flip_index is not None:
+            flip = self.action_data["requirements"][self.flip_index]["value"]
+            if flip:
+                print("Flipping")
+                new_transform.scale(-1, 1)
+                print(new_transform.m31())
+                print(new_transform.m32())
 
+                if hori_translation == 0 and vert_translation == 0:
+                    pass
+                else:
+                    new_transform.translate(
+                        hori_translation + new_transform.m31(),
+                        vert_translation - new_transform.m32()
+                    )
+
+        # Apply the new transform, and any changes made to it
+        self.setTransform(new_transform)
     def itemChange(self, change, value):
         """
         OVERRIDE: Called when the item has a change made to it. Currently, only selection changes are included
@@ -94,3 +128,10 @@ class SceneItem(QtWidgets.QGraphicsPixmapItem):
         self.data_changed_func(self)
 
         super().mouseReleaseEvent(event)
+
+    def AdjustTranslation(self, cur_m31, cur_m32, new_m31, new_m32) -> tuple:
+        pass
+        #if
+        #hori_translation + new_transform.m31(),
+        #vert_translation - new_transform.m32()
+
